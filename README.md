5b5cc5b59d18754a05157a4817c70801e3e51dfb
Piotr <piotrziobrowski03@gmail.com>
2023-04-11 23:04:57 +0200

    new program

--- README.md
+++
@@ -1,9 +0,0 @@
-<h1 align="center">C++ space</h1>
-
-<p align="center">
-  <img src="https://scontent-frt3-2.xx.fbcdn.net/v/t1.15752-9/339740837_1590934271381456_4128316134381362374_n.png?_nc_cat=100&ccb=1-7&_nc_sid=ae9488&_nc_ohc=Z-iohL8V9WUAX-oERsh&_nc_ht=scontent-frt3-2.xx&oh=03_AdRRQTaAcbG900zn6X2vLgMKkjEkjdyN__qGWYbgzhPcbQ&oe=645A8F65" width="65%" style="max-width: 480px;" frameBorder="0" class="giphy-embed" allowFullScreen></img>
-</p>
-
-<h3 align="center">This is the place where you can review my journey with the C++ language.</h3>
-
-<p align="center"> <a target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/cplusplus/cplusplus-original.svg" alt="cplusplus" width="40" height="40"/> </a> </p>
--- complex_number_calculator.cpp
+++ complex_number_calculator.cpp
@@ -1,102 +1,101 @@
 #include <iostream>
 using namespace std;
 
-class Imaginary{
-
+class Imaginary {
     int re_, im_;
 
 public:
-
-    Imaginary(int re=0, int im=0){
-        re_=re;
-        im_=im;
+    Imaginary(int re=0, int im=0) {
+        re_ = re;
+        im_ = im;
     }
 
-    int getRe(){
+    int getRe() {
         return re_;
     }
 
-    int getIm(){
+    int getIm() {
         return im_;
     }
 
-
-    void setRe(int re){
-        re_=re;
+    void setRe(int re) {
+        re_ = re;
     }
 
-    void setIm(int im){
-        im_=im;
+    void setIm(int im) {
+        im_ = im;
     }
 
-    Imaginary operator+(const Imaginary &x){
+    Imaginary operator+(const Imaginary& x) {
         Imaginary result;
         result.re_ = re_ + x.re_;
         result.im_ = im_ + x.im_;
         return result;
     }
 
-    Imaginary operator-(const Imaginary &x){
+    Imaginary operator-(const Imaginary& x) {
         Imaginary result;
         result.re_ = re_ - x.re_;
         result.im_ = im_ - x.im_;
         return result;
     }
 
-    Imaginary operator*(int q){
+    Imaginary operator*(int q) {
         Imaginary result;
         result.re_ = re_ * q;
         result.im_ = im_ * q;
         return result;
     }
-    //void display(){
-    //cout << re_ << "+" << im_<<"i"<<endl;
-    //}
 };
 
-int main(){
-
+int main() {
     int re1, re2, im1, im2, operation, liczba;
 
-    cout << "Podaj część rzeczywistą liczby zespolonej 1: ";
+    cout << "Enter the real part of complex number 1: ";
     cin >> re1;
     cout << endl;
-    cout << "Podaj część zespoloną liczby zespolonej 1: ";
+
+    cout << "Enter the imaginary part of complex number 1: ";
     cin >> im1;
     cout << endl;
-    cout << "Podaj część rzeczywistą liczby zespolonej 2: ";
+
+    cout << "Enter the real part of complex number 2: ";
     cin >> re2;
     cout << endl;
-    cout << "Podaj część zespoloną liczby zespolonej 2: ";
+
+    cout << "Enter the imaginary part of complex number 2: ";
     cin >> im2;
     cout << endl;
-    cout << "Wpisz 1 jeśli chcesz, aby funkcja wykonała dodawanie lub 2, jeśli odejmowanie lub 3, jeśli mnożenie przez stałą: ";
+
+    cout << "Type 1 for addition, 2 for subtraction or 3 for multiplication by a constant: ";
     cin >> operation;
     cout << endl;
 
-    Imaginary a{re1, im1};
-    Imaginary b{re2, im2};
+    Imaginary a{ re1, im1 };
+    Imaginary b{ re2, im2 };
     Imaginary c = a + b;
     Imaginary d = a - b;
 
-    if(operation==1){
+    if (operation == 1) {
         cout << c.getRe() << " + " << c.getIm() << "i" << endl;
     }
-    else if(operation==2){
+    else if (operation == 2) {
         cout << d.getRe() << " - " << d.getIm() << "i" << endl;
     }
-    else if(operation==3){
-        cout << "Podaj stałą całkowitą przez którą chcesz pomnożyć liczby zespolone: ";
+    else if (operation == 3) {
+        cout << "Enter an integer constant to multiply complex numbers by: ";
         cin >> liczba;
         cout << endl;
+
         Imaginary e = a * liczba;
         Imaginary f = b * liczba;
-        //f.display();
+
         cout << e.getRe() << " + " << e.getIm() << "i" << endl;
         cout << f.getRe() << " + " << f.getIm() << "i" << endl;
     }
     else {
-        cout << "Nie posiadam jeszcze takich działań" << endl;
+        cout << "I don't know how to do that yet." << endl;
     }
+
     return 0;
 }
